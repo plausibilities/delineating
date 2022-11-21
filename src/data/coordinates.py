b540c99edebf87fd902c4f2a171c5744409a2fb4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-11-21 20:14:37 +0000

    Add src/data/coordinates.py

---
+++ src/data/coordinates.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+class Coordinates:
+
+    def __init__(self, data: pd.DataFrame):
+        """
+
+        """
+
+        self.data = data
+
+    @staticmethod
+    def __levels() -> dict:
+
+        levels = ['Basement', 'Ground']
+        indices = range(len(levels))
+
+        return dict(zip(levels, indices))
+
+    def __counties(self) -> dict:
+
+        counties = self.data['county'].unique()
+        indices = range(len(counties))
+
+        return dict(zip(counties, indices))
+
+    def exc(self):
+
+        coordinates = {'Level': self.__levels, 'County': self.__counties()}
+
+        return coordinates
