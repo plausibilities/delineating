021bc32167632f95860ed3b4350692ec6640a2a1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-11-21 13:15:12 +0000

    Add src/data/radioactivity.py

---
+++ src/data/radioactivity.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import pymc as pm
+
+
+class Radioactivity:
+
+    def __init__(self):
+        """
+
+        """
+
+    @staticmethod
+    def __read() -> pd.DataFrame:
+
+        try:
+            data = pd.read_csv(filepath_or_buffer=pm.get_data('srrs2.dat'))
+        except FileNotFoundError as err:
+            raise Exception(err.strerror)
+
+        # ascertaining field name consistency
+        data.rename(mapper=str.strip, axis='columns', inplace=True)
+
+        return data
+
+    @staticmethod
+    def __expand(data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Structuring; concatenating the `pure state` & `pure county` codes
+            1. FIPS States: https://en.wikipedia.org/wiki/Federal_Information_Processing_Standard_state_code
+            2. FIPS Counties: https://en.wikipedia.org/wiki/List_of_United_States_FIPS_codes_by_county
+
+        :param data:
+        :return:
+        """
+
+        data.loc[:, 'fips'] = data.stfips.astype(str).str.zfill(2) + data.cntyfips.astype(str).str.zfill(3)
+
+        return data
+
+    def exc(self, focus: str = None):
+        """
+
+        :param focus:
+        :return:
+        """
