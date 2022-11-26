b7671261a7ba51ec214762a1e5c551a4aa7c5e08
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-11-26 02:23:59 +0000

    Add src/graphics/models.py

---
+++ src/graphics/models.py
@@ -0,0 +1,48 @@
+import os
+
+import pymc as pm
+import graphviz
+
+
+class Models:
+
+    def __init__(self, path: str):
+        """
+
+        :param path:
+        """
+
+        self.__path = path
+
+    def __draw(self, model: pm.Model, name: str) -> bool:
+        """
+
+        :param model:
+        :param name:
+        :return:
+        """
+
+        # The DAG
+        diagram = pm.model_graph.ModelGraph(model=model).make_graph()
+        diagram.node_attr.update(shape='circle')
+        diagram.graph_attr.update(size="5.7,5.9")
+
+        # Save
+        try:
+            diagram.save(os.path.join(self.__path, name))
+            return True
+        except OSError as err:
+            raise Exception(err.strerror)
+
+    def exc(self, model: pm.Model, name: str = 'model.gv') -> None:
+        """
+
+        :param model:
+        :param name:
+        :return:
+        """
+
+        drawn = self.__draw(model=model, name=name)
+
+        if drawn:
+            graphviz.render(engine='dot', format='pdf', filepath=os.path.join(self.__path, name))
