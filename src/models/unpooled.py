eb16288833eea893295a55a3494d85b41ede749b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-11-26 18:35:05 +0000

    Add src/models/unpooled.py: In progress.

---
+++ src/models/unpooled.py
@@ -0,0 +1,51 @@
+import logging
+
+import pandas as pd
+import pymc as pm
+
+
+class Unpooled:
+
+    def __init__(self, coords: dict, seed: int):
+        """
+
+        :param coords: Inference Data Coordinates
+        :param seed: Modelling seed
+        """
+
+        self.__coords = coords
+        self.__seed = seed
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n%(message)s\n%(asctime)s.%(msecs)03d\n',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
+
+    def __model(self, data: pd.DataFrame):
+
+        with pm.Model(coords=self.__coords) as model:
+
+            # the values of the <floor> field
+            levelcode = pm.Data(name='levelcode', value=data['floor'].values, dims='N', mutable=True)
+            self.__logger.info(levelcode.get_value().shape)
+            self.__logger.info(levelcode.type())
+
+            # the values of the <countyindex> field
+            countyindex = pm.Data(name='countyindex', value=data['countyindex'].values, dims='N', mutable=True)
+            self.__logger.info(countyindex.get_value().shape)
+            self.__logger.info(countyindex.type())
+
+            # <apriori> is probably has 85 x 2 elements because there are 85 distinct counties w.r.t. MN, and 2 distinct
+            # dwelling/floor levels.  Hence, 85 x 2 random values from a normal distribution
+            apriori = pm.Normal(name='apriori', mu=0.0, sigma=10.0, dims=('County', 'Level'))
+
+            # shape(mu) === shape(levelcode): <levelcode> is a N x 1 boolean object
+            mu = apriori[countyindex, levelcode]
+
+            # sigma ~ pm.Exponential(name=, lam=)
+            sigma = pm.Exponential('sigma', 1.0)
+            y = pm.Normal('y', mu=mu, sigma=sigma, observed=data['ln_radon'].values, dims='N')
+            self.__logger.info(y)
+
+            return model
