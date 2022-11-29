c5391335ef93e2bc11a39e195a2c679e49294ef4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-11-29 20:37:04 +0000

    Update src/models/vi.py

--- src/models/vi.py
+++ src/models/vi.py
@@ -31,7 +31,34 @@ class VI:
 
         with pm.Model(coords=self.__coords) as model:
 
-            pm.Data(name='levelcode', value=data['floor'].values, dims='N', mutable=True)
+            # The values of the <floor> field
+            #   self.__logger.info(levelcode.get_value().shape)
+            levelcode = pm.Data(name='levelcode', value=data['floor'].values, dims='N', mutable=True)
+
+            # The values of the <countyindex> field
+            #   self.__logger.info(countyindex.get_value().shape), self.__logger.info(countyindex.type())
+            countyindex = pm.Data(name='countyindex', value=data['countyindex'].values, dims='N', mutable=True)
+
+            # Random intercepts
+            mu_ = pm.Normal('intercept.mu', mu=0.0, sigma=10)
+            sigma_ = pm.Exponential('intercept.sigma', 1)
+            intercepts = pm.Normal('intercepts', mu=mu_, sigma=sigma_, dims='County')
+
+            # Common gradient
+            beta = pm.Normal('beta', mu=0.0, sigma=10.0)
+
+            # Model expected value
+            mu = intercepts[countyindex] + (beta * levelcode)
+            self.__logger.info('The shape of mu, whereby mu = intercepts[countyindex] + (beta * levelcode): %s',
+                               mu.eval().shape)
+
+            # Model error
+            # sigma ~ pm.Exponential(name=, lam=)
+            sigma = pm.Exponential('sigma', 1)
+
+            # Likelihood
+            likelihood = pm.Normal('likelihood', mu=mu, sigma=sigma, observed=data['ln_radon'].values, dims='N')
+            self.__logger.info(likelihood)
 
             return model
 
