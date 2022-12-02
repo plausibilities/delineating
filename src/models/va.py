d29e0e4d69709340612ea2662a29ec19cfc3110b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-12-02 14:47:10 +0000

    Update src/models/va.py

--- src/models/va.py
+++ src/models/va.py
@@ -31,6 +31,28 @@ class VA:
 
         with pm.Model(coords=self.__coords) as model:
 
+            levelcode = pm.MutableData(name='levelcode', value=data['floor'].values, dims='N')
+            countyindex = pm.MutableData(name='countyindex', value=data['countyindex'].values, dims='N')
+
+            # Random intercepts
+            intercept_mu = pm.Normal('intercept.mu', mu=0.0, sigma=10.0)
+            intercept_sigma = pm.Exponential('intercept.sigma', 1.0)
+            intercepts = pm.Normal('intercepts', mu=intercept_mu, sigma=intercept_sigma, dims='County')
+
+            # Random gradients
+            gradient_mu = pm.Normal('gradient.mu', mu=0.0, sigma=10.0)
+            gradient_sigma = pm.Exponential('gradient.sigma', 1.0)
+            gradients = pm.Normal('gradients', mu=gradient_mu, sigma=gradient_sigma, dims='County')
+
+            # Expected value
+            mu = intercepts[countyindex] + gradients[countyindex] * levelcode
+
+            # Model error
+            sigma = pm.Exponential('sigma', 1.0)
+
+            # Likelihood
+            likehood = pm.Normal('likelihood', mu=mu, sigma=sigma, observed=data['ln_radon'].values, dims='N')
+
             return model
 
     def exc(self, data: pd.DataFrame) -> pm.Model:
