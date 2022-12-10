0d00016008d4c5f3faa4bcf523c911a0f1853612
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-12-10 19:35:20 +0000

    Add src/models/mrva.py

---
+++ src/models/mrva.py
@@ -0,0 +1,46 @@
+import logging
+
+import pandas as pd
+import pymc as pm
+
+
+class MRVA:
+
+    def __init__(self, coords: dict, seed: int):
+        """
+
+        :param coords: Inference Data Coordinates
+        :param seed: Modelling seed
+        """
+
+        self.__coords = coords
+        self.__seed = seed
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n%(message)s\n%(asctime)s.%(msecs)03d\n',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
+
+    def __model(self, data: pd.DataFrame) -> pm.Model:
+        """
+
+        :param data:The data set being modelled.
+        :return:
+        """
+
+        with pm.Model(coords=self.__coords) as model:
+
+            return model
+
+    def exc(self, data: pd.DataFrame) -> pm.Model:
+        """
+
+        :param data: The data set being modelled.
+        :return:
+        """
+
+        # A Bayesian model: a varying intercept varying gradient model with non-centred parameters
+        model = self.__model(data=data)
+        
+        return model
